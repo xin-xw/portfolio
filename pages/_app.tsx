95fd3dec0dfdfc581a709c8348f0cb3be2cc1183
Xin Wang <xinxw1@gmail.com>
2023-01-24 07:40:31 -0800

    revert

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -59,6 +59,7 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
       <Head>
         <title>@xin</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+          <link rel="shortcut icon" href="/favicon.svg" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
